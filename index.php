30fe804e93072fa0447cb02dead6e7c9d9aff410
NandiniSHinduja <69416051+NandiniSHinduja@users.noreply.github.com>
2020-10-07 17:36:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>(NANDHINISHINDUJA)</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+<a href="https://www.wa4e.com/assn/rps/"
+ target="_blank">Specification for this Application</a>
+</p>
+</div>
+</body>
